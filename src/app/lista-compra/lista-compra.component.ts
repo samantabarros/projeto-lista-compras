690356527200399b7b1169d065ecca2073598b82
samantabarros <samantadesenvolvedora@gmail.com>
2025-04-13 22:27:13 -0300

    feat: adiciona items na lista

--- src/app/lista-compra/lista-compra.component.ts
+++ src/app/lista-compra/lista-compra.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemLista } from './item-lista';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-compra',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './lista-compra.component.html',
   styleUrl: './lista-compra.component.scss'
 })
 export class ListaCompraComponent {
+  item: string = '';
+  lista: ItemLista[] = [];
 
+  adicionarItem() {
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
+    //console.table(this.lista);
+
+    this.item = '';
+  }
 }
